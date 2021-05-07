e4cce1db475c95275e250ba2e54ffdc44dcf0a1d
Daniel Herrero <danielherrerohernando@gmail.com>
2021-05-07 12:10:26 +0200

    feat: adding data and tasks

---
+++ js/data.js
@@ -0,0 +1,153 @@
+const movies = [
+  {
+    "title": "Lawrence of Arabia",
+    "year": 1962,
+    "director": "David Lean",
+    "duration": "3h 36min",
+    "genre": [
+      "Adventure",
+      "Biography",
+      "Drama",
+      "History",
+      "War"
+    ],
+    "rate": 8.3
+  },
+  {
+    "title": "Double Indemnity",
+    "year": 1944,
+    "director": "Billy Wilder",
+    "duration": "1h 47min",
+    "genre": [
+      "Crime",
+      "Drama",
+      "Film-Noir",
+      "Mystery",
+      "Thriller"
+    ],
+    "rate": 8.3
+  },
+  {
+    "title": "Amadeus",
+    "year": 1984,
+    "director": "Milos Forman",
+    "duration": "2h 40min",
+    "genre": [
+      "Biography",
+      "Drama",
+      "History",
+      "Music"
+    ],
+    "rate": 8.3
+  },
+  {
+    "title": "Eternal Sunshine of the Spotless Mind",
+    "year": 2004,
+    "director": "Michel Gondry",
+    "duration": "1h 48min",
+    "genre": [
+      "Drama",
+      "Romance",
+      "Sci-Fi"
+    ],
+    "rate": 8.3
+  },
+  {
+    "title": "Taxi Driver",
+    "year": 1976,
+    "director": "Martin Scorsese",
+    "duration": "1h 53min",
+    "genre": [
+      "Crime",
+      "Drama"
+    ],
+    "rate": 8.3
+  },
+  {
+    "title": "To Kill a Mockingbird",
+    "year": 1962,
+    "director": "Robert Mulligan",
+    "duration": "2h 9min",
+    "genre": [
+      "Crime",
+      "Drama"
+    ],
+    "rate": 8.3
+  },
+  {
+    "title": "Dunkirk",
+    "year": 2017,
+    "director": "Christopher Nolan",
+    "duration": "1h 46min",
+    "genre": [
+      "Action",
+      "Drama",
+      "History",
+      "Thriller",
+      "War"
+    ],
+    "rate": 8.3
+  },
+  {
+    "title": "Full Metal Jacket",
+    "year": 1987,
+    "director": "Stanley Kubrick",
+    "duration": "1h 56min",
+    "genre": [
+      "Drama",
+      "War"
+    ],
+    "rate": 8.3
+  },
+  {
+    "title": "2001: A Space Odyssey",
+    "year": 1968,
+    "director": "Stanley Kubrick",
+    "duration": "2h 29min",
+    "genre": [
+      "Adventure",
+      "Sci-Fi"
+    ],
+    "rate": 8.3
+  },
+  {
+    "title": "Singin\" in the Rain",
+    "year": 1952,
+    "director": "Stanley Donen",
+    "duration": "1h 43min",
+    "genre": [
+      "Comedy",
+      "Musical",
+      "Romance"
+    ],
+    "rate": 8.3
+  },
+  {
+    "title": "Toy Story 3",
+    "year": 2010,
+    "director": "Lee Unkrich",
+    "duration": "1h 43min",
+    "genre": [
+      "Animation",
+      "Adventure",
+      "Comedy",
+      "Family",
+      "Fantasy"
+    ],
+    "rate": 8.3
+  },
+  {
+    "title": "Toy Story",
+    "year": 1995,
+    "director": "John Lasseter",
+    "duration": "1h 21min",
+    "genre": [
+      "Animation",
+      "Adventure",
+      "Comedy",
+      "Family",
+      "Fantasy"
+    ],
+    "rate": 8.3
+  }
+]
